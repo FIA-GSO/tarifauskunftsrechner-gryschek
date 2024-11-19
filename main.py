15dce242cf89aae39d1f27afead8938ccc346eaa
Gryschekgso <sarah.g49@gso.schule.koeln>
2024-11-19 11:39:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-preis_erwachsene = 5.0
-preis_kinder = 2.5
-preis_jugend = 3.5
-preis_premium = 3.0
-preis_premium_tag = 6
-preis_basis = 4.0
+float preis_erwachsene = 5.0
+float preis_erwachsene_tag = 10
+float preis_kinder = 2.5
+float preis_kinder_tag = 5
+float preis_jugend = 3.5
+float preis_jugend_tag = 6 
+float preis_premium = 3.0
+float preis_premium_tag = 6
+float preis_basis = 4.0
+float preis_basis_tag =
+float sekt = 0.75
+float zwischensumme = 0
+float endpreis = 0
 
 print("                             @                                                                ")
 print("       @@@@@@                                @                   @                            ")
@@ -31,7 +38,7 @@ while True:
         print(" Wenn Sie Basis-Mitglied sind, geben Sie 'b' ein.")
         print(" Wenn Sie kein Mitglied sind, drücken Sie eine beliebige andere Taste. ")
         antwort_rabatt = input()
-        if antwort_rabatt == "p":                                     #nested loop mit tages ticket ausgabe fehlt noch, maybe mit
+        if antwort_rabatt == "p":                                     #preise noch eintragen un vriablen erstellen für tagestickets
             print(" ### Eintritt Premium-Mitglied ### ")
             print(" Möchten sie ein Halbtages oder Tagesticket?")
             print(" Drücken sie 'h für ein Halbtages- und 'T' für ein Tagesticket")
@@ -40,29 +47,33 @@ while True:
                 print(" Preis: ", preis_premium_tag, " Euro ")
             elif antwort_ticket == "h":
                 print(" Preis: ", preis_premium, " Euro ")
+            print(" Möchten Sie für 0.75€ Aufpreis einen Sekt trinken? ")
+            print(" Drücken Sie die Taste 'j' wenn Sie einen Sekt möchten. ")
+            print(" Wenn Sie keinen Sekt möchten drücken Sie eine beliebige andere Taste. ")
+            antwort_sekt = input()
+            if antwort_sekt == "j":
+                    print("test ja")
+                    zwischensumme += sekt
         elif antwort_rabatt == "b":
             print(" ### Eintritt Basis-Mitglied ### ")
             print(" Preis: ", preis_basis, " Euro ")
         else:
             print(" ### Eintritt Erwachsene (voller Preis) ### ")
             print(" Preis: ", preis_erwachsene, " Euro")
-        print(" Möchten Sie für 0.75€ Aufpreis einen Sekt trinken? ")
-        print(" Drücken Sie die Taste 'j' wenn Sie einen Sekt möchten. ")
-        print(" Wenn Sie keinen Sekt möchten drücken Sie die Taste 'n' ")
-        antwort_sekt = input()
-        if antwort_sekt == "j":
-                print("test ja")
-        elif antwort_sekt == "n":
-            print("no")
+
     print(" ### Viel Spaß ### ")
     print(" Wollen Sie einen weiteren Tarif abfragen? ")
     antwort_abfrage = input()
     print(" Bitte drücken Sie die Taste 'j' für ja")
     print(" Wenn sie keinen weiteren Tarif erfragen möchten drücken Sie die Taste 'n' ")
     if antwort_abfrage == "j":
-        print("Starte neue Tarifabfrage")
+        print("Möchten Sie den Ticketpreis zur Gesamtsumme addieren?")
+        antwort_gesammtsumme = input()
+        if antwort_gesammtsumme == "j":
+            endpreis += zwischensumme
+        print("Starte neue Tarifabfrage") 
     
     if antwort_abfrage == "n":
-        endpreis = "1ticket und 1sekt "
-        print(" ### Ihr Preis Beträgt ")
+        endpreis
+        print(" ### Ihr Preis Beträgt var(endpreis)")
         break
